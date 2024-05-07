911f87acb3ec64a89f615f6e87fe715736d309ea
Gautham Elango <gautham.gg@gmail.com>
2024-05-08 00:09:24 +1000

    fix

---
+++ components/event.tsx
@@ -0,0 +1,17 @@
+export default function Event({
+	title,
+	description,
+	time,
+	category,
+}: { title: string; description: string; time: string; category: string }) {
+	return (
+		<div className="p-4 border-border border-y-[0.5px] hover:bg-hover">
+			<h2 className="text-lg font-bold text-white">{title}</h2>
+			<p className="mt-2 text-sm text-gray">{description}</p>
+			<div className="mt-4">
+				<span className="text-xs font-semibold text-gray">{time}</span>
+				<span className="text-xs font-semibold text-gray ml-4">{category}</span>
+			</div>
+		</div>
+	);
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,41 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    colors: {
-      background: "#17101f",
-      white: "#ffffff",
-      gray: "#9fa3af",
-    },
-    extend: {
-      fontFamily: {
-        system: ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', 'Helvetica', 'Arial', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"']
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		colors: {
+			background: "#17101f",
+			white: "#ffffff",
+			gray: "#9fa3af",
+			border: "#4c3a4ec0",
+			hover: "#1b1423",
+		},
+		extend: {
+			fontFamily: {
+				system: [
+					"-apple-system",
+					"BlinkMacSystemFont",
+					'"Segoe UI"',
+					"Roboto",
+					"Helvetica",
+					"Arial",
+					"sans-serif",
+					'"Apple Color Emoji"',
+					'"Segoe UI Emoji"',
+					'"Segoe UI Symbol"',
+				],
+			},
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
